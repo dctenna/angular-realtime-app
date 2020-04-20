4988a2d0f12153cf0a5d99985876e9e6273bdb76
Tetiana Teslenko <Tetiana_Teslenko@epam.com>
2020-04-20 19:34:25 +0300

    create angular real time app

---
+++ angular-realtime-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    SocketIoModule.forRoot(config),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
